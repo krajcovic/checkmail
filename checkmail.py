49b78116e86b265e692e360bf7de5294a2b1dd8d
Krajcovic <dusan.krajcovic@gmail.com>
2015-08-21 14:25:05 +0200

    Check email on mail.nitrok.cz

--- checkmail.py
+++ checkmail.py
@@ -3,18 +3,17 @@
 
 __author__ = 'krajcovic'
 
-
 from imapclient import IMAPClient
+import sys
 import time
+import getopt
 
 import RPi.GPIO as GPIO
 
 DEBUG = True
 
 HOSTNAME = 'mail.nitrok.cz'
-USERNAME = 'pi'
-PASSWORD = 'raspberry2014'
-MAILBOX = 'Maildir'
+MAILBOX = 'INBOX'
 
 NEWMAIL_OFFSET = 1   # my unread messages never goes to zero, yours might
 MAIL_CHECK_FREQ = 60 # check mail every 60 seconds
@@ -26,12 +25,13 @@ RED_LED = 23
 GPIO.setup(GREEN_LED, GPIO.OUT)
 GPIO.setup(RED_LED,GPIO.OUT)
 
-def loop():
-    server = IMAPClient(HOSTNAME, use_uid=True, ssl=True)
-    server.login(USERNAME, PASSWORD)
+def loop(username, password):
+    server = IMAPClient(HOSTNAME, use_uid=True)
+    print(username, password)
+    server.login(username, password)
 
     if DEBUG:
-        print('Logging in as ' + USERNAME)
+        print('Logging in as ' + username)
         select_info = server.select_folder(MAILBOX)
         print('%d messages in INBOX' % select_info['EXISTS'])
 
@@ -50,11 +50,37 @@ def loop():
 
     time.sleep(MAIL_CHECK_FREQ)
 
+def main(argv):
 
-if __name__ == '__main__':
+    username = None
+    password = None
+
+    try:
+        opts, args = getopt.getopt(argv, "hu:p:", ["user=", "pass="])
+    except getopt.GetoptError:
+        print(__file__, '-u <user> -p <password>' )
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print(__file__, '-u <user> -p <password>')
+            sys.exit()
+        elif opt in ("-u", "--user"):
+            username = arg
+        elif opt in ("-p", "--pass"):
+            password = arg
+
+    if username is None:
+        print(__file__, '-u <user> -p <password>' )
+        sys.exit(2)
+
     try:
         print('Press Ctrl-C to quit.')
         while True:
-            loop()
+            loop(username, password)
     finally:
         GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
